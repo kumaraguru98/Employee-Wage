dd4984bc24184de827e8f874c3f0192fd71b1481
baluguru <smkumarag@gmail.com>
2020-10-20 00:16:15 +0530

    added dailywagemessage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,9 +2,14 @@ welcome!
 #! /bin/bash -x
 ispresent=1
 randomcheck=$(($RANDOM%2))
+wageperhour=20
+fulldayhour=8
 if [ $randomcheck -eq 0 ]
 then
 	echo "employee absent"
+	wage=0
 else
 	echo "employee present"
+	wage=$(($wageperhour * $fulldayhour))
+
 fi
