69710386718487f63358266b1470879f81029307
baluguru <smkumarag@gmail.com>
2020-10-20 10:57:33 +0530

    added monthwage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,15 +2,20 @@ welcome!
 #! /bin/bash -x
 ispresent=1
 isfulltime=2
-wageperhour=20
-empcheck=$(($RANDOM%2))
-case $empcheck in
-$isfulltime)
-	wagehour=8
-$isparttime)
-	wagehour=4
-*)
-	wagehour=0
-esac
-wage=$(($wagehour * $wageperhour))
-
+totalsalary=0
+emppperhour=20
+noofworkingday=20
+for((day=1;day<=$noofworkingday;day++))
+do
+	empcheck=$(($RANDOM%2))
+	case $empcheck in
+		$isfulltime)
+			emphour=8
+		$isparttime)
+			emphour=4
+		*)
+			emphour=0
+		esac
+	salary=$(($emphour * $empperhour))
+	totalsalary=$(($totalsalary * $salary))
+done
