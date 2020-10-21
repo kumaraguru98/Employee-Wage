d24cf166f93bad45437ba91fb4d3c160f0147a6f
baluguru <smkumarag@gmail.com>
2020-10-21 09:10:01 +0530

    fuctioncodeforworkhour

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,7 +9,8 @@ monthwage=0
 totalworkingday=0
 totalemphr=0
 maxhrinmonth=100
-empcheck=$(($RANDOM%2))
+function workhour() {
+local empcheck=$1
 case $empcheck in
 	$isfulltime)
 		emphour=8
@@ -18,9 +19,12 @@ case $empcheck in
 	*)
 		emphour=0
 esac
+rcho $emphour
 while [[ $totalemphr -lt $maxhrinmonth -a $totalworkingday -lt $noofworkingday ]]
 do
 	((totalworkingday++))
+	empcheck=$(($RANDOM%2));
+	emphr="$( workhour $empcheck )"
 	totalemphr=$(( totalemphr+emphour ))
 	dailywage=$(( $emphr * $emprateperhour ))
 	monthlywage=$(( monthwage+dailywage ))
