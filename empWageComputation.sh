6e07a1c3ec4e9023dacd16fba3ac94ff5b32c9f1
baluguru <smkumarag@gmail.com>
2020-10-21 09:24:54 +0530

    dailywagewithtotalwage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,6 +10,8 @@ totalworkingday=0
 totalemphr=0
 maxhrinmonth=100
 empcheck=$(($RANDOM%2))
+function workhour() {
+localempcheck=$1
 case $empcheck in
 	$isfulltime)
 		emphour=8
@@ -18,12 +20,18 @@ case $empcheck in
 	*)
 		emphour=0
 esac
+echo $emphr
+}
 while [[ $totalemphr -lt $maxhrinmonth -a $totalworkingday -lt $noofworkingday ]]
 do
 	((totalworkingday++))
+	empcheck="$( workhour $empcheck )"
 	totalemphr=$(( totalemphr+emphour ))
 	dailywage=$(( $emphr * $emprateperhour ))
 	monthlywage=$(( monthwage+dailywage ))
 done
+totalwage=$(( totalemphr * emprateperhour ));
 echo dailywage of employee $dailywage
 echo monthwage of employee $monthlywage
+echo totalwage of emp;oyee $totalwage
+echo dailwage of alldays : ${dailywage[@]}
